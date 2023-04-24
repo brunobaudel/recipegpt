c2c08c4cbf9e388e5baa0c00968bef18af42e96f
Bruno Baudel <brunobaudel@gmail.com>
2023-04-24 16:14:02 -0300

    Ajusta a dependencia do repository

--- recipechat/src/main/java/com/mobsky/recipechat/data/repository/OpenAIGPTRepositoryImpl.kt
+++ recipechat/src/main/java/com/mobsky/recipechat/data/repository/OpenAIGPTRepositoryImpl.kt
@@ -2,21 +2,21 @@ package com.mobsky.recipechat.data.repository
 
 import com.estudos.network.util.getSuccessResultWrapper
 import com.estudos.network.util.result
+import com.mobsky.recipechat.data.network.OpenAIGPTNetwork
 import com.mobsky.recipechat.data.network.api.OpenAIGPT3NetworkApi
 import com.mobsky.recipechat.data.network.api.model.CompletionRequest
 import com.mobsky.recipechat.data.repository.mapper.toRecipe
 import com.mobsky.recipechat.domain.model.Recipe
 
-
 class OpenAIGPTRepositoryImpl(
-    private val openAIGPTNetwork: OpenAIGPT3NetworkApi
+    private val openAIGPTNetwork: OpenAIGPTNetwork
 ) : OpenAIGPTRepository {
 
     override suspend fun getCompletion(requestBody: CompletionRequest): List<Recipe> = result {
         openAIGPTNetwork
             .getCompletion(requestBody)
+            .getSuccessResultWrapper()
             .toRecipe()
     }
-
 
 }
